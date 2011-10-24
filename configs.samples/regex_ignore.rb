ce668df7897801cd4fa1905dba2c8b58223b02a9
Daemon Le <qdaemon@gmail.com>
2011-10-24 08:03:49 -0700

    Some mode to regex and note to indicate any info methods can be set for regex ignore.

--- configs.samples/regex_ignore.rb
+++ configs.samples/regex_ignore.rb
@@ -1,9 +1,8 @@
 # Ruby code.
 #
-# regex_ignore is used for certain methods (cpuinfo, env, and sysctl_a) to
-#   specifically ignore certain data that was retrieved as they are deemed
-#   not relevant or would cause unnecessary diff (hence false reporting) to
-#   be generated.
+# regex_ignore is used for certain methods (all info_methods) to specifically
+#   ignore certain data that was retrieved as they are deemed not relevant or
+#   would cause unnecessary diff (hence false reporting) to be generated.
 #
 
 # Declaration.  Always needed.
@@ -13,16 +12,16 @@ $regex_ignore = {}
 # Pick and choose what to override, or comment out what shouldn't be ...
 #
 
-$regex_ignore['cpuinfo'] = %r{
+$regex_ignore['get_cpuinfo'] = %r{
   (
-      bogomips
-    | cpu\ MHz
+      ^bogomips
+    | ^cpu\ MHz
   )
 }x
 
-$regex_ignore['env'] = %r{
+$regex_ignore['get_env'] = %r{
   ^(
-      _
+      ^_$
     | DISPLAY
     | OLDPWD
     | XAUTHORITY
@@ -30,15 +29,15 @@ $regex_ignore['env'] = %r{
   )$
 }x
 
-$regex_ignore['sysctl_a'] = %r{
+$regex_ignore['get_sysctl_a'] = %r{
   (
       ^error:
-    | fs.dentry-state
-    | fs.file-nr
-    | fs.inode-(nr|state)
-    | fs.quota.syncs
-    | kernel.pty.nr
-    | kernel.random.(boot_id|entropy_avail|uuid)
+    | ^fs.dentry-state
+    | ^fs.file-nr
+    | ^fs.inode-(nr|state)
+    | ^fs.quota.syncs
+    | ^kernel.pty.nr
+    | ^kernel.random.(boot_id|entropy_avail|uuid)
     | \.netfilter.ip_conntrack_count
     | \.random
     | \.route.gc_timeout
